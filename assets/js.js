4c315156a011c8610d4feffd3d963540c546de8c
jobeuse <iradukundajob3@gmail.com>
2022-12-15 15:23:17 +0200

    adding responsive

--- assets/js.js
+++ assets/js.js
@@ -33,3 +33,46 @@ var form=document.getElementById("form")
 
 });
  
+
+
+    var burger_button= document.getElementById("burger_button"); 
+    var navbar= document.getElementById("NavItems_icons"); 
+    var close= document.getElementById("close"); 
+
+function showNavabar (){ 
+    burger_button.addEventListener("click", function (){
+        navbar.style.display="block";  
+        burger_button.style.display="none"
+        close.style.display="block";
+    });
+
+   
+   
+}
+
+
+function closeNavbar (){ 
+     close.style.display="none";
+
+     if(close){ 
+         close.addEventListener("click", function (){
+                navbar.style.display="none"; 
+                burger_button.style.display="block"
+                  close.style.display="none";
+             });
+    }
+
+}
+
+
+showNavabar();
+closeNavbar();
+
+    // function myFunction() {
+    //     var x = document.getElementById("myTopnav");
+    //     if (x.className === "topnav") {
+    //         x.className += "reponsive";
+    //     } else {
+    //         x.className = "topnav";
+    //     }
+    //     }
